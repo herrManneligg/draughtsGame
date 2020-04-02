621d97e103d9c5f61d028a36df922b2a6932d87c
Pablo Berjon <2490772B@student.gla.ac.uk>
2020-04-02 10:03:51 +0100

    Renamed some files and deleted unnused classes

--- src/Model.java
+++ src/Model.java
@@ -6,10 +6,9 @@ public class Model {
 	ImageIcon imageWhite = new ImageIcon("white.png");;
 	ImageIcon imageRed = new ImageIcon("red.png");;
 	ImageIcon imageWhiteKing = new ImageIcon("white_king.png");;
-	ImageIcon imageRedKing = new ImageIcon("Red_king.png");;
+	ImageIcon imageRedKing = new ImageIcon("red_king.png");;
 	
 	public Model() {
-
 	}
 	
 	public ImageIcon getRedKing() {
@@ -26,18 +25,5 @@ public class Model {
 	
 	public ImageIcon getWhite() {
 		return this.imageWhite;
-	}
-	
-//	Refactor
-	public ImageIcon getToken(int n) {
-		if (n == 0) {
-			return this.imageRed;
-		} else if (n == 1) {
-			return this.imageRedKing;
-		} else if (n == 2) {
-			return this.imageWhite;
-		} else {
-			return this.imageWhiteKing;
-		}
 	}
 }
